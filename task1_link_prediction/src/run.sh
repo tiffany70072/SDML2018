e5edb8fe0e2a5316114a20c3f9915497e82464e0
tiffany70072 <tiffany70072@gmail.com>
2019-10-10 23:55:08 +0800

    Update run.sh

--- task1_link_prediction/src/run.sh
+++ task1_link_prediction/src/run.sh
@@ -4,6 +4,6 @@ python3 get_data.py
 time python3 main.py word2vec regcl 2-1-0 30
 python3 data/pred-txt-to-csv.py results/word2vec_regcl_2-1-0.txt
 
-#cd ../../ML1_2
-#python3 test_random.py
-#cd ../task2/src
+# cd ../../ML1_2
+# python3 test_random.py
+# cd ../task2/src
